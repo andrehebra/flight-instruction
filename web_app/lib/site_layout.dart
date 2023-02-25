b7c418432f789a9301564dda7f5887547d420631
andrehebra <andrehebra@gmail.com>
2023-02-25 02:43:05 -0500

    created appbar separate widget

--- web_app/lib/site_layout.dart
+++ web_app/lib/site_layout.dart
@@ -2,17 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:web_app/helpers/responsive.dart';
 import 'package:web_app/widgets/large_screen.dart';
 import 'package:web_app/widgets/small_screen.dart';
+import 'package:web_app/widgets/top_nav.dart';
 
 class SiteLayout extends StatelessWidget {
-  const SiteLayout({super.key});
+  SiteLayout({super.key});
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.black,
-      ),
+      appBar: topNavigationBar(context, scaffoldKey),
       body: ResponsiveWidget(
         largeScreen: LargeScreen(),
         smallScreen: SmallScreen(),
