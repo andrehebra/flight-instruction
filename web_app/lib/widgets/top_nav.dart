06a3f508aa7abd6878f9b02b1d2bb6ecab1376b1
andrehebra <andrehebra@gmail.com>
2023-02-25 13:49:29 -0500

    working on adding logo to assets, not working

--- web_app/lib/widgets/top_nav.dart
+++ web_app/lib/widgets/top_nav.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:web_app/helpers/responsive.dart';
+import '../constants/style.dart';
 
 AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
     AppBar(
+      leading: !ResponsiveWidget.isSmallScreen(context)
+          ? Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Image.asset("assets/logo/airplane.svg"),
+                )
+              ],
+            )
+          : IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu),
+            ),
       elevation: 0,
       backgroundColor: Colors.black,
     );
