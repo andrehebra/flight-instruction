a72313d744523e31909998a2bd92f027613f1fe2
andrehebra <52555960+andrehebra@users.noreply.github.com>
2023-02-25 22:49:26 -0500

    added horizontal menu item code

--- web_app/lib/main.dart
+++ web_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:web_app/site_layout.dart';
 
 void main() {
+  Get.put(MenuController());
   runApp(MyApp());
 }
 
---
+++ web_app/lib/widgets/horizontal_menu_items.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
+
+import '../constants/controllers.dart';
+import '../constants/style.dart';
+import 'custom_text.dart';
+
+class HorizontalMenuItem extends StatelessWidget {
+  final String itemName;
+  final Function onTap;
+
+  const HorizontalMenuItem(
+      {super.key, required this.itemName, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    return InkWell(
+      onTap: onTap,
+      onHover: (value) {
+        value
+            ? menuController.onHover(itemName)
+            : menuController.onHover("not hovering");
+      },
+      child: Obx(() => Container(
+            color: menuController.isHovering(itemName)
+                ? lightGrey.withOpacity(0.1)
+                : Colors.transparent,
+            child: Row(
+              children: [
+                Visibility(
+                  visible: menuController.isHovering(itemName) ||
+                      menuController.isActive(itemName),
+                  maintainSize: true,
+                  maintainState: true,
+                  maintainAnimation: true,
+                  child: Container(
+                    width: 6,
+                    height: 40,
+                    color: dark,
+                  ),
+                ),
+                SizedBox(
+                  width: _width / 80,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: menuController.returnIconFor(itemName),
+                ),
+                if (!menuController.isActive(itemName))
+                  Flexible(
+                      child: CustomText(
+                          text: itemName,
+                          color: menuController.isHovering(itemName)
+                              ? dark
+                              : light))
+                else
+                  Flexible(
+                      child: CustomText(
+                    text: itemName,
+                    color: dark,
+                    size: 18,
+                    weight: FontWeight.bold,
+                  ))
+              ],
+            ),
+          )),
+    );
+  }
+}
